bdf5e5bb87f901bb9923228c9ace1afceb46dddc
Nicadeo <shoeske6@gmail.com>
2025-06-02 14:22:05 +0300

    Create simplest_calculator.py

---
+++ simplest_calculator.py
@@ -0,0 +1,18 @@
+#простейший калькулятор
+a=float(input("Введите первое число: "))
+b=float(input("Введите второе число: "))
+result=(0)
+operation=input("Выберите операцию (+, -, *, /): ")
+arr = ["+","-","/","*"]
+if operation == "+":
+    result = a+b
+if operation == "-":
+    result = a*b
+if operation == "*":
+    result = a*b
+if operation == "/":
+    result = a/b
+if operation not in arr:
+    print("Неверная операция!")
+if operation in arr:
+    print("Результат:", result)
